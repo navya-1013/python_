9e6a1f84cbcaa5012f09eb3e74406510d0ef004b
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-07-07 14:33:12 +0530

    Update Concatenation.py

--- Day-1/Concatenation.py
+++ Day-1/Concatenation.py
@@ -1,3 +1,5 @@
-print("Hello" + "Navya")
+print("Hello" + "Navya") #This will combine both the strings like HelloNavya
 
-print("123" + "456")
+print("123" + "456") #This will combine both the strings like 123456
+
+print(123 + 456) #This will add both the numbers 
