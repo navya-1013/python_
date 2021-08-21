c015309a42fa71c05ed85151b53b0cb9beff5fb4
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-08-21 13:48:23 +0530

    Update print individual character of a string.py

--- Day-2/print individual character of a string.py
+++ Day-2/print individual character of a string.py
@@ -1,4 +1,12 @@
 #Pulling the particular element from a string is called "Subscript"
 #Data Types
 
-print("Hello"[0])
+print("Hello"[0]) //H
+
+print("Hello"[1]) //e
+
+print("Hello"[2]) //l
+
+print("Hello"[3]) //l
+
+print("Hello"[4]) //o
