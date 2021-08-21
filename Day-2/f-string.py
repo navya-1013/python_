f29e8d4fed342ae16ac6537f363120420e55e3b3
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-08-21 15:42:27 +0530

    Create f-string.py

---
+++ Day-2/f-string.py
@@ -0,0 +1,4 @@
+score = 0
+height = 1.8
+isWinning = True
+print(f"your score is {score}, your height is {height}, you are winning is {is Winning}") //your score is 0, your height is 1.8, you are winning is True
