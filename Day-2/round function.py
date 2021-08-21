dfac07c8034040e62666b22e7a9d5fc7048dd202
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-08-21 15:30:41 +0530

    Update round function.py

--- Day-2/round function.py
+++ Day-2/round function.py
@@ -2,4 +2,4 @@
 print(round(8 / 3)) //3
 
 #If we want decimal place values to show after using round function, we have to use the , and number of decimal place values we want i.e, 2 or 3
-print(round(8 / 3)) //2.67
+print(round(8 / 3, 2)) //2.67
