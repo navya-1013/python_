1a38f478fc7e0a9e076225439012ddc9452c5a89
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-08-21 14:32:05 +0530

    Update String not with integer.py

--- Day-2/String not with integer.py
+++ Day-2/String not with integer.py
@@ -1,4 +1,5 @@
 #Data Types
 
 num_char = len(input("What's your name?"))
-print("Your name has " + num_char + "characters.")
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
