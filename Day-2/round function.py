f521ff5479a73dae1f2335d19b568b4d7ec491cb
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-08-21 15:30:31 +0530

    Update round function.py

--- Day-2/round function.py
+++ Day-2/round function.py
@@ -1,2 +1,5 @@
 #with the round function the result of mathematical operator has float number then it will change to integer
-print(round(8/ 3)) //3
+print(round(8 / 3)) //3
+
+#If we want decimal place values to show after using round function, we have to use the , and number of decimal place values we want i.e, 2 or 3
+print(round(8 / 3)) //2.67
