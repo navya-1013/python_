2c169c30460e5930694e8b2f8c35a092e5bf64f8
navya-1013 <86942861+navya-1013@users.noreply.github.com>
2021-08-21 15:33:24 +0530

    Update Add mathematical operators.py

--- Day-2/Add mathematical operators.py
+++ Day-2/Add mathematical operators.py
@@ -6,6 +6,9 @@ print(3 * 2) //6
 
 print(6 / 3) //2.0
 
+#if we want a value as integer not as float integer then use floor division (//)
+print(8 // 3) //2
+
 print(2 ** 3) //8
 
 print(3 * 3 + 3 / 3 - 3) //7.0
